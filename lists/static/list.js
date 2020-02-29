e38550f84869b0275e0fb27bb4712f6500b1e725
Yeri <arifyeripratama@gmail.com>
2020-02-29 21:00:13 +0700

    use list.js in real thing

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,7 @@
-var initialize = function () {
-  $('input[name="text"]').on('keypress', function () {
-    $('.has-error').hide();
-  });
-};
+/* global window, $ */
+window.Superlists = {}
+window.Superlists.initialize = function() {
+  $('input[name="text"]').on('keypress', function() {
+    $('.has-error').hide()
+  })
+}
